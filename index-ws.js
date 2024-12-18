997f20aac190b726689c78b9097192df6c3c15ea
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-12-18 12:09:02 +0100

    Express websocket

--- index-ws.js
+++ index-ws.js
@@ -10,3 +10,30 @@ server.on("request", app);
 server.listen(3000, function () {
   console.log("server started on port 3000");
 });
+
+/** Begin WebSocket */
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log("Clients connected", numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitors: ${numClients}`);
+    console.log("A client has disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function eact(client) {
+    client.send(data);
+  });
+};
